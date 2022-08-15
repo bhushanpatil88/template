59210661520e64a382739b86f789a765996e5851
Bhushan <76675802+bhushanpatil88@users.noreply.github.com>
2022-08-15 13:40:42 +0530

    added debuger for all container

--- template.cpp
+++ template.cpp
@@ -13,17 +13,36 @@ using namespace std;
 #define all(v) v.begin(),v.end()
 #define endl "\n"
 #define fastio  cin.tie(0), cout.tie(0),ios_base::sync_with_stdio(false);
-#define debug(...) __f(#__VA_ARGS__,__VA__ARGS)
+
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+
+
+
 //===============================================Functions=============================================
 
-template <typename Arg1>
-void __f(const char* name,Arg1&& arg1){cout<<name<<" : "<<arg1<<endl;}
-template <typename Arg1,typename... Args>
-void __f(const char* names,Arg1&& arg1,Args&&... args)
-{
-  const char* comma = strchr(names+1,',');
-  cout.write(names,comma-names)<<" : "<<arg1<<" | ";__f(comma+1,args...);
-}
 
 
 
