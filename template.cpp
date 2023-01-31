7b76ac431ba03f11e72f8a2c06027f028b3c0d48
Bhushan <76675802+bhushanpatil88@users.noreply.github.com>
2023-01-31 20:28:07 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,18 +1,27 @@
+#pragma GCC optimize("O3,unroll-loops")
+
 #include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
 using namespace std;
+using namespace __gnu_pbds;
 //==============================================DEFINES=============================================
 #define int   long long 
 #define M 1000000007 //1e9+7
 #define pi 3.1415926535897932384626
 #define ff first
 #define ss second
+#define set_bits __builtin_popcountll
 #define vi vector<int>
 #define input(v) for(int &c:v)cin>>c;
-#define vpi vector<pair<int,int>>
-#define inputvpi(v)  for (int i = 0; i < n; i++){int x,y; cin>>x>>y; v.push_back({x,y}); }
 #define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
 #define endl "\n"
 #define fastio  cin.tie(0), cout.tie(0),ios_base::sync_with_stdio(false);
+
+// pbds find_by_order, order_of_key
+typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; 
 
 
 #ifndef ONLINE_JUDGE
@@ -37,54 +46,20 @@ template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_prin
 template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
 template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
 template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
-
-
+void _print(pbds v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
 
 
 //===============================================Functions=============================================
 
-
-
-
 //Modular subtraction
-  int modsub(int a,int b,int c)
-   { return ((a%c) - (b%c) + c)%c;}
- 
- 
- //Modular Power
-  int modpow(int x,int n,int c)
-  {
-    int result=1;
-    while(n>0) {if(n & 1) result=(result * x)%c ; x=(x*x) % c ; n>>=1;}
- 
-    return result;
-  }
-
-
-   //LCM
-  int lcm(int a, int b)
-  {
-    return (a / __gcd(a, b)) * b;
-  }
-
-
- //Modular Inverse (x is  inverse of b under modulo m)
- int modInverse(int b, int m) 
- { 
-     return modpow(b,m-2,m); //Fermat's little theorem if(M = prime) else extended_gcd
- } 
-  
-  
- //MOdular Division
- int modDivide(int a, int b, int m) 
- { 
-     a = a % m; 
-     int inv = modInverse(b, m); 
-     if (inv == -1) 
-        return -1; 
-     else
-        return (inv * a) % m; 
- } 
+int modsub(int a,int b,int c){ return ((a%c) - (b%c) + c)%c;}
+//Modular Power
+int modpow(int x,int n,int c){int result=1;while(n>0) {if(n & 1) result=(result * x)%c ; x=(x*x) % c ; n>>=1;}return result;}   //LCM
+int lcm(int a, int b){return (a / __gcd(a, b)) * b;}
+//Modular Inverse (x is  inverse of b under modulo m)
+int modInverse(int b, int m) {return modpow(b,m-2,m);} //Fermat's little theorem if(M = prime) else extended_gcd 
+//MOdular Division
+ int modDivide(int a, int b, int m){a = a % m;int inv = modInverse(b, m);if (inv == -1)return -1;else return (inv * a) % m;} 
 
 const int N = 1e5+10;
 const int INF=1e9+10;
@@ -104,7 +79,7 @@ void sol(){
 //===================================================Main==============================================
 signed main() {
   
-	 fastio;
+     fastio;
     int T=1;
     cin>>T;
    
